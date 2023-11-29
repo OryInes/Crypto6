8e068973fe7be4a8acff8894085f5e4ab5be2f4a
OryInes <152093402+OryInes@users.noreply.github.com>
2023-11-29 15:45:07 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,56 +1,32 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 
 using namespace std;
 
-// Fonction pour chiffrer une lettre selon le chiffrement de César
-char chiffrementCesar(char lettre, int decalage) {
-    if (isalpha(lettre)) {
-        char base = isupper(lettre) ? 'A' : 'a';
-        return static_cast<char>((lettre - base + decalage) % 26 + base);
-    }
-    return lettre;  // Ne pas chiffrer les caractères non alphabétiques
-}
-
-// Fonction pour chiffrer un mot
-string chiffrementCesarMot(const string& mot, int decalage) {
-    string motChiffre = "";
-    for (char caractere : mot) {
-        motChiffre += chiffrementCesar(caractere, decalage);
-    }
-    return motChiffre;
-}
-
-// Fonction pour chiffrer une chaîne de caractères
-string chiffrementCesarTexte(const string& texte, int decalage) {
-    stringstream ss(texte);
-    string mot;
-    string texteChiffre = "";
-
-    while (ss >> mot) {
-        texteChiffre += chiffrementCesarMot(mot, decalage) + " ";
-    }
-
-    return texteChiffre;
-}
-
 int main() {
-    // Exemple d'utilisation
-    string texte;
-    int decalage;
-
-    cout << "Entrez la clé de chiffrement de César : ";
-    cin >> decalage;
-    cin.ignore();  // Ignorer le caractère de nouvelle ligne restant dans le buffer
-
     cout << "Entrez le texte à chiffrer : ";
+    string texte;
     getline(cin, texte);
 
-    // Appel de la fonction de chiffrement de César pour le texte entier
-    string texteChiffre = chiffrementCesarTexte(texte, decalage);
+    cout << "Entrez la clé A : ";
+    int cleA;
+    cin >> cleA;
+
+    cout << "Entrez la clé B : ";
+    int cleB;
+    cin >> cleB;
+
+    string texteChiffre;
+    for (int i = 0; i < texte.length(); i++) {
+        char lettre = texte[i];
+        int code = lettre - 'a';
+        int codeChiffre = (cleA * code + cleB) % 26;
+        lettre = 'a' + codeChiffre;
+        texteChiffre += lettre;
+    }
 
     cout << "Texte chiffré : " << texteChiffre << endl;
 
     return 0;
 }
+
